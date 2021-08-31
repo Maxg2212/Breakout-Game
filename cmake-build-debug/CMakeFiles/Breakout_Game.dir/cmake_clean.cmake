d1f51a88bc8d97d8e241987a53a63d077f023153
Maxg2212 <maxgarro22@estudiantec.cr>
2021-08-30 20:41:35 -0600

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Breakout_Game.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Breakout_Game"
+  "Breakout_Game.pdb"
+  "CMakeFiles/Breakout_Game.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Breakout_Game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Breakout_Game.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Breakout_Game.
+# This may be replaced when dependencies are built.

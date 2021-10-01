c65b5281eb5c5014ffa44d56973910af581c37e4
maicolvn1399 <maicolvn1399@gmail.com>
2021-09-30 18:47:04 -0600

    [Update] Comments added to SocketServer class

--- SocketServer/SocketServer.h
+++ SocketServer/SocketServer.h
@@ -26,26 +26,49 @@ struct dataSocket{
     sockaddr_in info;
 };
 
+/**
+ * @file SocketServer.h
+ * @authors Michael Valverde Navarro
+ * @brief Clase que se encarga de ser el servidor y realizar una conexión con el cliente
+ * @version 2.0
+ * @date 3/09/2021
+ */
 class SocketServer{
 public:
+    /**
+     * @brief Constructor del server
+     */
     SocketServer();
+    /**
+     * @brief Corre el servidor y establece una conexión con el cliente
+     */
     void run();
+    /**
+     * @brief Envia un mensaje al cliente
+     * @param msn puntero de const char que contiene el mensaje que se quiere enviar
+     */
     void setMensaje(const char* msn);
     static string receivedMessage;
-
 
 private:
     int descriptor; //Identifica clientes
     sockaddr_in info; //informacion del socket servidor
     vector<int> clientes; //almacena clientes
 
+    /**
+     * @brief Permite crear el socket que se va utilizar en la conexión
+     * @return true si permite crear el socket, false en caso contrario
+     */
     bool crear_socket();
+    /**
+     * @brief Establece la conexión con el cliente
+     * @return true si permite establecer conexión, false en caso contrario
+     */
     bool enlazar_kernel();
+    /**
+     * @brief Funciona como controlador de flujo de información para el servidor
+     */
     void* ContoladorCliente(void* obj);
-
-
-
-
 };
 
 #endif //BREAKOUT_GAME_SOCKETSERVER_H
